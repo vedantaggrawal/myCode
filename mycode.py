1d563acbc93cb511cec201e2231b956a3a08489b
Vedant Aggrawal <vedant.aggrawal@gmail.com>
2021-05-24 18:48:59 +0530

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -4,10 +4,10 @@ import mysql.connector as mysql
 class Tree:
 
     db = mysql.connect(
-        host = "localhost",
-        user = "",
-        passwd = "",
-        database = "neutron"
+        host = "192.168.62.11",
+        user = "root",
+        passwd = "xE2j!b5pi2S-D641WW8fnLWNZBmdR-N2",
+        database = "octavia"
         )
 
     _flag = False
@@ -21,62 +21,62 @@ class Tree:
             self.label = label
             self.children = children
 
-    def print_tree(self,parent_id,parent_name="loadbalancer"):
-
+    def print_tree(self,parent_id,parent_name="load_balancer"):
         if not self.children:
-            print self.label.upper()
+            print(self.label.upper())
             if Tree._flag:
                   self.delete_info(parent_id,parent_name)
             else:
-                  print self.show_info(parent_id,parent_name)
+                  print(self.show_info(parent_id,parent_name))
 
 
         else:
 
             for node_id in self.get_root_id(parent_id,parent_name):
                 node_id=node_id[0]
-                print node_id,type(node_id)
+                #print(node_id,type(node_id))
 
                 for child in self.children:
                     child.print_tree(node_id,self.label)
 
-                print self.label.upper()
+                print(self.label.upper())
 
             if Tree._flag:
                   self.delete_info(parent_id,parent_name)
             else:
-                  print self.show_info(parent_id,parent_name)
+                  print(self.show_info(parent_id,parent_name))
 
     def get_root_id(self,parent_id,parent_name):
-        #print parent_name
+        #print(parent_name)
         cursor = Tree.db.cursor()
-        if self.label == "lbaas_loadbalancers":
+        if self.label == "load_balancer":
                 cursor.execute("select id from {} where name='{}'".format(self.label,parent_id))
         else:
-                cursor.execute("select id from {} where {}_id='{}'".format(self.label,parent_name.split("_")[1][:-1],parent_id))
+        	cursor.execute("select id from {} where {}_id='{}'".format(self.label,parent_name,parent_id))
         return cursor.fetchall()
 
     def show_info(self,parent_id,parent_name):
-        #print parent_name
+        #import pdb;pdb.set_trace()
+        #print(parent_name)
         cursor = Tree.db.cursor()
-        if self.label == "lbaas_loadbalancers":
-                cursor.execute("select * from {} where name='{}'".format(self.label,parent_id))
+        if self.label == "load_balancer":
+                cursor.execute("select id from {} where name='{}'".format(self.label,parent_id))
         else:
-                cursor.execute("select * from {} where {}_id='{}'".format(self.label,parent_name.split("_")[1][:-1],parent_id))
+        	cursor.execute("select * from {} where {}_id='{}'".format(self.label,parent_name,parent_id))
         return cursor.fetchall()
 
     def delete_info(self,parent_id,parent_name):
         cursor = Tree.db.cursor()
-        if self.label == "lbaas_loadbalancers":
+        if self.label == "load_balancer":
                 cursor.execute("delete from {} where name='{}'".format(self.label,parent_id))
                 Tree.db.commit()
         else:
-                cursor.execute("delete from {} where {}_id='{}'".format(self.label,parent_name.split("_")[1][:-1],parent_id))
+                cursor.execute("delete from {} where {}_id='{}'".format(self.label,parent_name,parent_id))
  #       return cursor.fetchall()
 
 
 if __name__ == "__main__":
 
-    n = Tree("lbaas_loadbalancers",[Tree("lbaas_loadbalanceragentbindings"),Tree("lbaas_loadbalancer_statistics"),Tree("lbaas_listeners",[Tree("lbaas_sni"),Tree("lbaas_l7policies",[Tree("lbaas_l7rules")])]),Tree("lbaas_pools",[Tree("lbaas_members"),Tree("lbaas_sessionpersistences")])])
+    n = Tree("load_balancer",[Tree("amphora"),Tree("vip"),Tree("vrrp_group"),Tree("listener",[Tree("sni"),Tree("l7policy",[Tree("l7rule")])]),Tree("pool",[Tree("member"),Tree("session_persistence"),Tree("health_monitor")])])
     n.set_flag(sys.argv[1])
     n.print_tree(sys.argv[2])
